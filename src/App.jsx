78a6d9856e9a67860d6ab8286fb3dde288243327
Uday Bhatiya <139557497+uday-bhatiya@users.noreply.github.com>
2024-06-15 06:45:57 +0000

    name, links

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Uday Bhatiya",
+  title: "Web Developer",
+  email: "udaybhatiya31@gmail.com",
+  gitHub: "uday-bhatiya",
+  instagram: "udaybhatiyaa",
+  linkedIn: "uday-bhatiya",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "udaybhatiyaa",
+  youTube: " ",
 };
 
 const primaryColor = "#4E567E";
